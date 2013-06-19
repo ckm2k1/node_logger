8d6a291791eccede62f4b5bfc510c04d6274fed7
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2013-06-19 10:11:01 -0400

    adding the basics for loggin messages and dependencies

---
+++ logger.js
@@ -0,0 +1,35 @@
+var util = require('util'),
+  colors = require('colors'),
+  _ = require('underscore');
+
+var logDefaults = {
+  showHidden: true,
+  depth: null,
+  colors: true,
+  debug: true
+};
+var log = function (obj, options) {
+  options = options || {};
+  _.defaults(options, logDefaults);
+  if (!options.debug) return;
+  console.log(util.inspect(obj, options));
+};
+
+var logWithColor = function(msg, color) {
+  console.log(msg[color]);
+};
+
+var warn = function(msg) {
+  logWithColor(msg, 'yellow');
+};
+var error = function(msg) {
+  logWithColor(msg, 'red');
+};
+var success = function(msg) {
+  logWithColor(msg, 'green');
+};
+
+module.exports.log = log;
+module.exports.error = error;
+module.exports.success = success;
+module.exports.warn = warn;
